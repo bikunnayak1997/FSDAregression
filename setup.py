0641a2d949e01b487fef402a64e2cbbd86ebeb02
bikunnayak1997 <bikun.nayak2212@gmail.com>
2023-04-09 02:55:44 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+##make to floder into package
+
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+      
+    return requirements
+
+setup(
+    name='RegressorProject',
+    version='0.0.1',
+    author='Bikun',
+    author_email='bikun.nayak2212@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+#only install package
